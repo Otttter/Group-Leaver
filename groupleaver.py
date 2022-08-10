42704c8fd4820707ef2803d7ebbcd6dd8a4c7e90
risq <98481603+risq56@users.noreply.github.com>
2022-08-11 01:22:44 +0200

    Add files via upload

--- groupleaver.py
+++ groupleaver.py
@@ -1,28 +1,30 @@
 import discord
 import asyncio
+from discord.ext import commands
+
 
 #
 # Any issues feel free to message my discord Otter#7070
 #
 
-client = discord.Client()
-token = "" # Put your token here
+bot = commands.Bot(command_prefix='!', self_bot=True)
+token = "" #<<--- put your token here
 prefix = "#"
 command = "gl"
 leaveMessage = "Bye!"
 
-@client.event
+@bot.event
 async def on_ready():
     print("Token Verification Successful! Type " + prefix + "" + command + " in to any chat and the script will execute!") # Tell the user the script is actually running.
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         cmd = str(message.content).split(' ')
         if cmd[0] == prefix + command:
             await message.delete()
             count = 0
-            for channel in client.private_channels:
+            for channel in bot.private_channels:
                 if isinstance(channel, discord.GroupChannel):
                     if channel.id != message.channel.id: # If the message was sent in a group chat, dont leave it.
                         count = count + 1
@@ -30,7 +32,6 @@ async def on_message(message):
                         await channel.leave()
                         print("Left a group: " + str(channel.id)) # Print group ID in console.
             await message.channel.send("``You left a total of [" + str(count) + "] group chats!``")
-            await client.close() # Updated because they changed it for some reason
-
-client.run(token, bot=False)
+            await bot.close() # Updated because they changed it for some reason
+bot.run(token, bot=False)
 input("Press enter to exit") # Allow user to actually fucking read the data before the script closes.
